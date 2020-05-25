06be5eb899909eaa1eda363db2c4fb75c049cf3c
josh <josh2013.880922@gmail.com>
2020-05-26 07:53:19 +0800

    admin article menagement

--- src/com/controller/ArticleController.java
+++ src/com/controller/ArticleController.java
@@ -21,14 +21,12 @@ public class ArticleController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		int id = -1;//articleId;
+		
 		try {
 			ArticleDAO articleDAO = new ArticleDAO();
-			ArrayList<Article> titleList = articleDAO.getAuditedArticleInfos();
-
-			id = (request.getParameter("id") == null) ? titleList.get(0).getId()
-					: Integer.parseInt(request.getParameter("id"));
+			ArrayList<Article> titleList = (request.getParameter("action").equals())
+			id = (request.getParameter("id") == null) ? titleList.get(0).getId(): Integer.parseInt(request.getParameter("id"));
 			Article a = articleDAO.getArticle(id);
-
 			request.setAttribute("article", a);// 要顯示的文章
 			request.setAttribute("titleList", titleList);// 要顯示的文章清單
 		} catch (Exception e) {
